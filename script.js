eabd4f8f38d22a50ec9f86a66536462d194568ac
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2022-04-29 17:34:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const step = .75;
+
+// ---------- PEOPLE -----------
+
+const tall = document.querySelector(".tall"), short = document.querySelector(".short"), lamp = document.querySelector(".lamp");
+
+// tall.style.left = getComputedStyle(tall).left, short.style.left = getComputedStyle(short).left; // in px
+
+document.onmousedown = ()=>{setin = setInterval(move, 100);};
+document.onmouseup = ()=>{clearInterval(setin);};
+
+function move() {
+    tall.style.left = parseFloat(tall.style.left)+step+'%';
+    short.style.left = parseFloat(short.style.left)-step+'%';
+    calcShadow()
+}
+
+
+// ---------- SHADOWS --------
+
+const tallS = tall.querySelector(".shadow"), shortS = short.querySelector(".shadow");
+tallS.style.width = shortS.style.width = '0px';
+
+function calcShadow() {
+    l = parseInt(getComputedStyle(lamp).height);
+    hT = parseInt(getComputedStyle(tall).height);
+    hS = parseInt(getComputedStyle(short).height);
+    d = parseInt(getComputedStyle(tall).left) - parseInt(getComputedStyle(lamp).left);
+
+    tallS.style.width = d*hT/(l-hT)+'px';
+    shortS.style.width = d*hS/(l-hS)+'px';
+}
+
+
+// ------- HELPERS --------
+
+function reset() {
+    tallS.style.width = shortS.style.width = '0px';
+    tall.style.left = short.style.left = lamp.style.left;
+    // document.querySelector('body').style.transform = 'scale(.8)';
+}
+
+
