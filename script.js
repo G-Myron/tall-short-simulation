4a4fdd46a55521eae6a89f0dc613cc0b8582ffff
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2022-04-30 23:03:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const tall = document.querySelector(".tall"), short = document.querySelector(".s
 
 // tall.style.left = getComputedStyle(tall).left, short.style.left = getComputedStyle(short).left; // in px
 
-document.onmousedown = document.ontouchstart = ()=>{setin = setInterval(move, 60);};
-document.onmouseup = document.ontouchend = ()=>{clearInterval(setin);};
+const touchTarget = document.querySelector(".container")
+
+touchTarget.onmousedown = touchTarget.ontouchstart = ()=>{setin = setInterval(move, 60);};
+touchTarget.onmouseup = touchTarget.ontouchend = ()=>{clearInterval(setin);};
 
 function move() {
     tall.style.left = parseFloat(tall.style.left)+step+'%';
@@ -38,6 +40,11 @@ function reset() {
     tallS.style.width = shortS.style.width = '0px';
     tall.style.left = short.style.left = lamp.style.left;
     // document.querySelector('body').style.transform = 'scale(.8)';
+}
+
+
+function resize(value) {
+    document.querySelector('html').style.setProperty("--lamp-height", value+'vh');
 }
 
 
